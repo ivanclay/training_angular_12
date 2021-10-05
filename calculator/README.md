20221c00a2ba9a34fa70b1075804b40fdf85a4fe
ivanclay <ivan.clay.moura@gmail.com>
2021-10-05 13:40:23 -0300

    Configuring tests.

--- calculator/README.md
+++ calculator/README.md
@@ -12,6 +12,7 @@ A simple calculator Web as a way to study Angular.
 - [ ] Install dependencies
     - [x] bootstrap
 - [x] Configuring bootstrap in ***'angular.json'***
+- [x] Configuring tests
  
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.8.
 
--- calculator/src/app/calculator/services/calculator.service.spec.ts
+++ calculator/src/app/calculator/services/calculator.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
 
@@ -13,4 +13,40 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be 1 + 4 = 5', ()=> {
+      inject([CalculatorService], (service: CalculatorService)=> {
+        let soma = service.calcule(1,4,CalculatorService.ADD);
+        expect(soma).toEqual(5);
+      })
+  });
+
+  it('should be 1 - 4 = -3', ()=> {
+    inject([CalculatorService], (service: CalculatorService)=> {
+      let sub = service.calcule(1,4,CalculatorService.SUBTRACT);
+      expect(sub).toEqual(-3);
+    })
+  });
+
+  it('should be 1 * 4 = 4', ()=> {
+    inject([CalculatorService], (service: CalculatorService)=> {
+      let multi = service.calcule(1,4,CalculatorService.MULTIPLY);
+      expect(multi).toEqual(4);
+    })
+  });
+
+  it('should be 1 / 4 = 0.25', ()=> {
+    inject([CalculatorService], (service: CalculatorService)=> {
+      let divide = service.calcule(1,4,CalculatorService.DIVIDE);
+      expect(divide).toEqual(0.25);
+    })
+  });
+
+  it('should be 0 to invalid operation', ()=> {
+    inject([CalculatorService], (service: CalculatorService)=> {
+      let invalidOp = service.calcule(1,4,'%');
+      expect(invalidOp).toEqual(0);
+    })
+  });
+
 });
