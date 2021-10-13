7d391b72b2fe627191081f691328df54e4aa87b5
ivanclay <ivan.clay.moura@gmail.com>
2021-10-13 16:09:36 -0300

    initial commit

--- currency-converter/src/app/converter/services/converter.service.ts
+++ currency-converter/src/app/converter/services/converter.service.ts
@@ -12,7 +12,8 @@ import {
 })
 export class ConverterService {
 
-  private readonly BASE_URL = "http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3";
+  // private readonly BASE_URL = "http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3";
+  private readonly BASE_URL = "http://data.fixer.io/api/latest?access_key=f1ac211370a6f58860aa5a27355dd061";
 
   constructor(private http: HttpClient) { }
 
